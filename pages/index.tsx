08db810417e8f6a5ef5f4ef57c011b352c46e98b
Matheus Bastos <matheusbastos@outlook.com>
2020-11-06 11:29:22 -0300

    first commit, project config

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+import { AppProps } from "next/app";
+import Head from "next/head";
+
+const Home: FC = () => {
+  return (
+    <>
+      <Head>
+        <title>TODO List</title>
+      </Head>
+
+      <main></main>
+    </>
+  );
+};
+
+export default Home;
